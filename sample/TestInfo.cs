b0deef9bbbfa1e243d54a815c419b184ccca7be5
Stephane Delcroix <stephane@mono-cvs.ximian.com>
2008-04-02 07:29:03 +0000

    2008-04-02  Stephane Delcroix  <sdelcroix@novell.com>
    	* configure.in:
    	* Makefile.am: 
    	* sample:
    	* sample/TestInfo.cs:
    	* sample/Makefile.am: a working sample.
    
    svn path=/trunk/gio-sharp/; revision=99612

---
+++ sample/TestInfo.cs
@@ -0,0 +1,26 @@
+using GLib;
+using System;
+using System.Text;
+
+namespace TestGio {
+	public class TestInfo {
+		static void Main (string[] args)
+		{
+			if (args.Length != 1) {
+				Console.WriteLine ("Usage: TestInfo <uri>");
+				return;
+			}
+		
+			Gtk.Application.Init ();
+
+			File file = FileFactory.NewForUri (args[0]);
+			FileInfo fileinfo = (file as File).QueryInfo ("*",FileQueryInfoFlags.None, null);
+			Console.WriteLine (fileinfo.Name);
+			Console.WriteLine (fileinfo.EditName);
+			Console.WriteLine (fileinfo.ContentType);
+			Console.WriteLine (fileinfo.Size);
+			Console.WriteLine (fileinfo.IsHidden);
+			Console.WriteLine (fileinfo.IsSymlink);
+		}
+	}
+}
